47c4ff083b062cc9dbf2e2cbc2e31fd4bef7dca8
takudzwa <shumbamhinit@gmail.com>
2018-11-20 15:35:57 +0200

    Added view and template for detailed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url('', views.post_list, name='post_list'),
 ]
-
